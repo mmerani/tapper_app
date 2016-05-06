2d22f2ca68ff8afcd7ae3468760dcb4e956a82fc
Michael Merani <michaelmerani@michaels-mbp.attlocal.net>
2016-05-06 14:09:23 -0400

    added Tapper files

--- tapper/ViewController.swift
+++ tapper/ViewController.swift
@@ -9,17 +9,61 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    //Variables
+    var taps_max: Int = 0
+    var taps_curr: Int = 0
+    //My Outlets
+    @IBOutlet weak var logoImg: UIImageView!
+    @IBOutlet weak var howManyTapsTxt: UITextField!
+    @IBOutlet weak var playButton: UIButton!
+    
+    @IBOutlet weak var tapButton: UIButton!
+    @IBOutlet weak var tapsLabel: UILabel!
+    
+    @IBAction func OnCoinTap(sender: UIButton){
+        taps_curr += 1
+        updateTaps()
+        if Gameover(){
+            Restart()
+        }
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func OnPlay(sender: UIButton!){
+        if howManyTapsTxt.text != nil && howManyTapsTxt.text != ""
+        {
+            logoImg.hidden = true
+            playButton.hidden = true
+            howManyTapsTxt.hidden = true
+            tapButton.hidden = false
+            tapsLabel.hidden = false
+            
+            taps_max = Int(howManyTapsTxt.text!)!
+            taps_curr = 0
+            updateTaps()
+            
+        }
+     }
+    func Restart(){
+        taps_max = 0
+        howManyTapsTxt.text = ""
+        logoImg.hidden = false
+        playButton.hidden = false
+        howManyTapsTxt.hidden = false
+        tapButton.hidden = true
+        tapsLabel.hidden = true
+        
     }
-
-
+    func Gameover() -> Bool{
+        if taps_curr >= taps_max{
+            return true
+        }
+        else{
+            return false
+        }
+    }
+    func updateTaps(){
+        tapsLabel.text = "\(taps_curr) Taps"
+    }
 }
 
